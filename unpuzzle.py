ff0a9322c3f35f447d7c2f700c87657c66972eb0
Brenda <101911555+Khenya@users.noreply.github.com>
2024-03-18 12:12:53 -0400

    Update unpuzzle.py

--- unpuzzle.py
+++ unpuzzle.py
@@ -2,9 +2,14 @@ import cv2
 import numpy as np
 import mediapipe as mp
 import random
-import os
-os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 
+npiezas = 3
+patito = npiezas * npiezas
+
+# ganaste???
+def win():
+    global stack,shl
+    shl[stack[0]],shl[stack[1]]
 
 # Swap two frames in the image
 def swap():
@@ -12,41 +17,44 @@ def swap():
     if len(stack)==2:
         shl[stack[0]],shl[stack[1]]=shl[stack[1]],shl[stack[0]]
         stack=[]
+        win()
 
 # Identify spatial location and swap frames
 def spacial_location_check(cx,cy):
     global x
     chk=-1
-    for i in range(5):
-        for j in range(5):
+    for i in range(npiezas):
+        for j in range(npiezas):
             chk+=1
             if cx in range(bxs[j,i,0],bxl[j,i,0]) and cy in range(bxs[j,i,1],bxl[j,i,1]):
                 x.append([i,j])
                 if x[0]!=x[-1]:
                     x=[]
-                elif len(x)==25:
+                elif len(x)==patito:
                     stack.append(chk)
                     if len(stack)>1:
                         swap()
                     x=[]
 
 if __name__ == '__main__':
-    bxs = np.full((5, 5, 2), 0)
-    bxl = np.full((5, 5, 2), 0)
+    bxs = np.full((npiezas, npiezas, 2), 0)
+    bxl = np.full((npiezas, npiezas, 2), 0)
     si = 600
-    bl = [0, si // 5, (si // 5) * 2, (si // 5) * 3, (si // 5) * 4, si]  # Divide en 5 partes iguales
-    for i in range(5):
-        for j in range(5):
+    bl = [0, si // npiezas, (si // npiezas) * 2, (si // npiezas) * 3, (si // npiezas) * 4, si]  # Divide en 5 partes iguales
+    for i in range(npiezas):
+        for j in range(npiezas):
             bxs[i, j] = (bl[i], bl[j])
             bxl[i, j] = (bl[i + 1], bl[j + 1])
 
         
     # Load Puzzle image, split image into pieces and randomly shuffle the frames
     imgx = cv2.imread(r'img.jpg')
+    fin = cv2.VideoCapture(r'boom.gif')
+    
     imgx = cv2.resize(imgx,(si,si), fx = 0.1, fy = 0.1)
     stack,x,shl=[],[],[]
-    for i in range(5):
-            for j in range(5):
+    for i in range(npiezas):
+            for j in range(npiezas):
                 shl.append(imgx[bl[i]:bl[i+1],bl[j]:bl[j+1]])
     random.shuffle(shl)
 
@@ -62,16 +70,20 @@ if __name__ == '__main__':
     # Drawing function of hand landmarks on the image
     mp_drawing = mp.solutions.drawing_utils
 
+    
+
     while True:
         # Capture and process image frame from video
+        success, f = fin.read()
+
         success, image = cap.read()
         image=cv2.flip(image,1)
         image = cv2.resize(image,(si+200,si), fx = 0.1, fy = 0.1)
         imageRGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         results = hands.process(imageRGB)
         ck=0
-        for i in range(5):
-            for j in range(5):
+        for i in range(npiezas):
+            for j in range(npiezas):
                 if ck in stack:
                     image[bl[i]:bl[i+1],bl[j]:bl[j+1]]=cv2.blur(shl[ck],ksize=(15,15))
                 else:
@@ -96,7 +108,6 @@ if __name__ == '__main__':
                         cv2.circle(image, (cxp, cyp), 25, (255, 0, 0), cv2.FILLED)
                     
                     distance = np.sqrt((cxi - cxp)**2 + (cyi - cyp)**2)
-                    # print (distance)
                     if distance < 50 and distance > 0:
                         spacial_location_check(cxi,cyi)
                         cv2.circle(image, (cxi, cyi), 25, (0, 0, 255), cv2.FILLED)
@@ -104,6 +115,7 @@ if __name__ == '__main__':
 
                 mp_drawing.draw_landmarks(image, handLms, mp_hands.HAND_CONNECTIONS)
 
+        cv2.imshow("output", f)
         cv2.imshow("output", image)
         if (cv2.waitKey(1) & 0xFF == ord('q')):
                 break
